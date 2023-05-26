40b91a347189cf1fc9a1135f933d7b270a3bac35
hankun <ihankun@outlook.com>
2023-05-27 00:39:45 +0800

    添加组件

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import Antd from 'ant-design-vue';
 // import 'ant-design-vue/dist/antd.less';
+import http from './http/index'
+
 import Vue from 'vue';
 Vue.use(Antd);
 export {
-    
+    http
 }
